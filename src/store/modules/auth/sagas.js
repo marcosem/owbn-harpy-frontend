9a6ce3d3941a26f6e72d06c5ca9d74b96455ebae
Marcos Mathias <marcos-github@memathias.com>
2019-08-30 18:22:36 -0300

    Added Reducers

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,88 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    // Add the token to api header
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+    history.push('/dashboard');
+  } catch (err) {
+    if (err.response) {
+      toast.error(`Error: ${err.response.data.error}`);
+      console.tron.error(err.response.data.error);
+    } else {
+      toast.error(
+        "Authentication failure! e-mail and/or password doesn't match!"
+      );
+      console.tron.error(err);
+    }
+
+    yield put(signFailure());
+  }
+}
+
+/*
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+    });
+
+    history.push('/');
+    toast.success('Your account was successfully created!');
+  } catch (err) {
+    if (err.response) {
+      toast.error(`Error: ${err.response.data.error}`);
+      console.tron.error(err.response.data.error);
+    } else {
+      toast.error('Register failure! Verify your data!');
+      console.tron.error(err);
+    }
+
+    // Log the error if any
+
+    yield put(signFailure());
+  }
+}
+*/
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    // Add the token to api header
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  // takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,36 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, '/users', profile);
+
+    toast.success('Profile successfully updated!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    if (err.response.data.error) {
+      toast.error(`Error: ${err.response.data.error}`);
+      console.tron.error(err.response.data.error);
+    } else {
+      toast.error('Unable to update profile, verify your data!');
+      console.tron.error(err);
+    }
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
